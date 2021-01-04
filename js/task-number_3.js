c23378cf91bed7b3ecccd66c3a51fc20f7d4c038
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2021-01-04 20:03:16 +0200

    ДЗ 4

---
+++ js/task-number_3.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function add(n) {
+    let result = 0;
+
+    function addToResult(num) {
+        if (num === undefined) return result;
+
+        result += num;
+
+        return addToResult;
+    }
+    return addToResult(n);
+}
+
+console.log(add());
+
+console.log(add(2)(5)(7)());
